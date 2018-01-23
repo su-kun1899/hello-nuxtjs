ba37fb8fd5fb5f1bc4e0a3a5274e9d440e46d3be
su-kun1899 <higedrum.coz@gmail.com>
2018-01-23 18:11:48 +0900

    add hello world

---
+++ pages/hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="demo">
+    <p>{{message}}</p>
+    <input v-model="message">
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'hello world!'
+    }
+  }
+}
+</script>
