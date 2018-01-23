4d24fb24a6e4d343d45112aa962d7f0e9c089906
su-kun1899 <higedrum.coz@gmail.com>
2018-01-23 19:58:42 +0900

    add mikan

---
+++ components/mikan.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ name }} は {{ price }}円</p>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      name: 'みかん',
+      price: 100
+    }
+  }
+}
+</script>
+
---
+++ pages/fruits.vue
@@ -0,0 +1,11 @@
+<template>
+  <mikan/>
+</template>
+
+<script>
+import mikan from '~/components/mikan'
+
+export default {
+  components: { mikan }
+}
+</script>
