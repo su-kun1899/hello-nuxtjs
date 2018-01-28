9b1461b48932bb827f956809f3aeb04deb05eb85
su-kun1899 <higedrum.coz@gmail.com>
2018-01-28 21:01:01 +0900

    Get$B%Q%i%a!<%?$NCM$r2hLL$K=P$7$F$_$k(B

--- components/Name.vue
+++ components/Name.vue
@@ -2,8 +2,23 @@
   <div v-bind:class="{'error':!isValid}" v-bind:style="{'border': (isValid ? '' : '1px solid red')}">
     <p id="name">名前: {{name}}</p>
     <input v-model="name">
+    <p>
+      <!-- onInputでnameの値を書き換える -->
+      <input type="text" v-on:input="name = $event.target.value">
+    </p>
+    <p>
+      <!-- onChangeでnameの値を書き換える -->
+      <input type="text" v-on:change="name = $event.target.value">
+    </p>
+    <p>
+      <!-- onChangeでメソッドを呼び出す -->
+      <input type="text" v-on:change="showName($event)">
+    </p>
     <p id="message" v-show="!isValid">
       名前を入力して下さい。
+    </p>
+    <p>
+      パラメータ: {{params}}
     </p>
   </div>
 </template>
@@ -12,12 +27,18 @@
 export default {
   data: function() {
     return {
-      name: ''
+      name: '',
+      params: 'default'
     }
   },
   computed: {
     isValid: function() {
       return this.name.length > 0
+    }
+  },
+  methods: {
+    showName: function(event) {
+      alert(event.target.value)
     }
   }
 }
--- pages/name.vue
+++ pages/name.vue
@@ -1,10 +1,26 @@
 <template>
+  <div>
   <Name/>
+  hoge: {{hoge}}
+  fuga: {{fuga}}
+  </div>
 </template>
 
 <script>
 import Name from '~/components/Name'
 export default {
-  components: { Name }
+  data: function() {
+    return {
+      hoge: 'aaa',
+      fuga: 'bbb'
+    }
+  },
+  components: { Name },
+  asyncData({ query }) {
+    return {
+      hoge: 'ワイワイ',
+      fuga: query['fuga']
+    }
+  }
 }
 </script>
