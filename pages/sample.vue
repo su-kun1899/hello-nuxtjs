7889b8862d72d5f08efaec43b53be511386ad760
su-kun1899 <higedrum.coz@gmail.com>
2018-01-29 10:22:43 +0900

    asyncDataでqueryを表示するサンプル

---
+++ pages/sample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>hoge: {{hoge}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      // asyncDataで上書きされる
+      hoge: 'default'
+    }
+  },
+  asyncData(context) {
+    return {
+      // asyncDataでreturnすると、dataにマージされる
+      hoge: context.query['hoge']
+    }
+  }
+}
+</script>
